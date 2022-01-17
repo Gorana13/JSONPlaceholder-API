f3cba4cb376614cd012acaf5fe67aaad9980f99a
Gorana13 <97852897+Gorana13@users.noreply.github.com>
2022-01-17 12:29:21 +0100

    Added validation rules to edit form

--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -8,13 +8,21 @@
     >
       <v-card>
         <v-card-text>
-          <form @submit.prevent="onSubmit" method="patch">
+          <v-form @submit.prevent="onSubmit" ref="form" method="patch">
             <v-text-field v-model="id" label="ID" readonly> </v-text-field>
-            <v-text-field label="Naziv" v-model="title"></v-text-field>
-            <v-text-field v-model="body" label="Text"></v-text-field>
+            <v-text-field
+              label="Naziv"
+              :rules="[rules.required, rules.counter]"
+              v-model="title"
+            ></v-text-field>
+            <v-text-field
+              v-model="body"
+              :rules="[rules.required]"
+              label="Text"
+            ></v-text-field>
             <v-btn type="submit"> submit </v-btn>
             <v-btn @click.prevent="close()"> Close </v-btn>
-          </form>
+          </v-form>
         </v-card-text>
       </v-card>
     </v-dialog>
@@ -44,6 +52,10 @@ export default {
       id: null,
       title: null,
       body: null,
+      rules: {
+        required: (value) => !!value || "Required.",
+        counter: (value) => value?.length <= 20 || "Max characters is 20",
+      },
     };
   },
   watch: {
@@ -61,6 +73,9 @@ export default {
       this.$emit("closeEvent");
     },
     onSubmit() {
+      if (!this.$refs.form.validate()) {
+        return;
+      }
       this.item = {
         id: this.id,
         title: this.title,
